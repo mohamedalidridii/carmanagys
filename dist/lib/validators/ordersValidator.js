e24542dbf68d40719fbe9112b32934b9f207b772
mohamedalidridii <mohamedali.dridi1@esprit.tn>
2023-12-22 17:46:35 +0100

    22-12-2023 5:46(change module)

--- dist/lib/validators/ordersValidator.js
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-export var OrdersValidator = z.object({
-    userId: z.string(),
-    agent: z.string(),
-    produit: z.string(),
-    lubrifiant: z.string(),
-    points: z.string(),
-    total: z.string(),
-});
--- dist/lib/validators/query-validator.js
+++
@@ -1,6 +0,0 @@
-import { z } from "zod";
-export var QueryValidator = z.object({
-    category: z.string().optional(),
-    sort: z.enum(['asc', 'desc']).optional(),
-    limit: z.number().optional(),
-});
