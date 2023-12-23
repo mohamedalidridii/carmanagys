4ab0d29fbe662de048102058bf80bc5ba599fe71
mohamedalidridii <mohamedali.dridi1@esprit.tn>
2023-12-23 15:40:46 +0100

    23-12-2023 2:09 (testing route trpc and waiting for the end of trpc)

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -3,13 +3,13 @@ import { fetchRequestHandler } from '@trpc/server/adapters/fetch'
 
 const handler = (req: Request) => {
   console.log('this is an api request')
-  fetchRequestHandler({
+  return fetchRequestHandler({
     endpoint: '/api/trpc',
     req,
     router: appRouter,
     // @ts-expect-error context already passed from express middleware
     createContext: () => ({}),
-  })
+  });
   console.log('this is the end of api request')
 }
 
