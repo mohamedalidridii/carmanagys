50f896318159b7e3f564dcf8c95aaf2ed7c1bd51
mohamedalidridii <mohamedali.dridi1@esprit.tn>
2023-12-23 12:13:29 +0100

    23-12-2023 12:13(payload-utils)

--- src/lib/payload-utils.ts
+++ src/lib/payload-utils.ts
@@ -1,23 +1,24 @@
-import {ReadonlyRequestCookies} from "next/dist/server/web/spec-extension/adapters/request-cookies"
-import { NextRequest } from "next/server"
 import { User } from '../cms-types'
-
-
+import { ReadonlyRequestCookies } from 'next/dist/server/web/spec-extension/adapters/request-cookies'
+import { NextRequest } from 'next/server'
 
 export const getServerSideUser = async (
-    
-    cookies: NextRequest["cookies"] | ReadonlyRequestCookies
-    ) => {
-        console.log('server is working')
-        const token = cookies.get("payload-token")?.value
-        
-        const meRes = await fetch(
-            `${process.env.NEXT_PUBLIC_SERVER_URL}/api/users/me`,
-             {
-            headers: {
-                Authorization: `JWT ${token}`,
-            },
-        })
-    const {user}= (await meRes.json()) as {user: User | null} 
-    return {user}
+  cookies: NextRequest['cookies'] | ReadonlyRequestCookies
+) => {
+  const token = cookies.get('payload-token')?.value
+
+  const meRes = await fetch(
+    `${process.env.NEXT_PUBLIC_SERVER_URL}/api/users/me`,
+    {
+      headers: {
+        Authorization: `JWT ${token}`,
+      },
+    }
+  )
+
+  const { user } = (await meRes.json()) as {
+    user: User | null
+  }
+
+  return { user }
 }
