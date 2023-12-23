0cee7e51c0ee675e165d9ab879c3f4a965505541
mohamedalidridii <mohamedali.dridi1@esprit.tn>
2023-12-23 13:24:07 +0100

    23-12-2023 12:30 (to ensure that the authentication and redirects are handled correctly )

--- src/lib/payload-utils.ts
+++ src/lib/payload-utils.ts
@@ -1,24 +1,42 @@
 import {ReadonlyRequestCookies} from "next/dist/server/web/spec-extension/adapters/request-cookies"
 import { NextRequest } from "next/server"
 import { User } from '../cms-types'
-
+import { NextApiRequest, NextApiResponse } from 'next';
 
 
 export const getServerSideUser = async (
     
-    cookies: NextRequest["cookies"] | ReadonlyRequestCookies
+    req: NextApiRequest,
+    res: NextApiResponse
     ) => {
-        console.log('server is working correctly')
-        const token = cookies.get("payload-token")?.value
-        
-        const meRes = await fetch(
-            `${process.env.NEXT_PUBLIC_SERVER_URL}/api/users/me`,
-             {
-            headers: {
-                Authorization: `JWT ${token}`,
-            },
-        })
-        console.log('meRes:', meRes);
-    const {user}= (await meRes.json()) as {user: User | null} 
-    return {user}
+        const token = req.cookies['payload-token'];
+
+        if (!token) {
+          // Redirect to sign-in page if the token is not present
+          res.redirect('/sign-in?origin=cart');
+          return { user: null };
+        }
+      
+        const meRes = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/users/me`, {
+          headers: {
+            Authorization: `JWT ${token}`,
+          },
+        });
+      
+        if (!meRes.ok) {
+          console.error('Error fetching user data:', meRes.statusText);
+          // Handle the error, return a default user, or throw an exception as needed.
+          res.status(500).json({ error: 'Internal Server Error' });
+          return { user: null };
+        }
+      
+        try {
+          const { user } = await meRes.json();
+          return { user };
+        } catch (error) {
+          console.error('Error parsing JSON:', error);
+          // Handle the JSON parsing error.
+          res.status(500).json({ error: 'Internal Server Error' });
+          return { user: null };
+        }
 }
