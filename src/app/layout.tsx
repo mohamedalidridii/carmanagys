d47acd831108b824f60a92b7fd14b1cde66a1939
mohamedalidridii <mohamedali.dridi1@esprit.tn>
2023-12-23 14:54:29 +0100

    23-12-2023 2:09 (check providers and navbar)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,13 +21,13 @@ export default function RootLayout({
     <html lang="en" className='h-full'>
       <body className={cn("relative h-full font-sans antialiased", inter.className)}>
         <main className='relative flex flex-col min-h-screen '>
-          <Providers>
-          <NavBar/>
+          {/* <Providers> */}
+          {/* <NavBar/> */}
             <div
               className='flex-grow flex-1'
             >{children}
             </div>
-          </Providers>
+          {/* </Providers> */}
         </main> 
         <Toaster position='top-center' richColors/>
       </body>
