0f557ad9047ff0422145453e93d3612a1d5ae3ba
mohamedalidridii <mohamedali.dridi1@esprit.tn>
2023-12-23 13:33:38 +0100

    23-12-2023 12:30 (go back to previous)

--- src/lib/payload-utils.ts
+++ src/lib/payload-utils.ts
@@ -6,37 +6,19 @@ import { NextApiRequest, NextApiResponse } from 'next';
 
 export const getServerSideUser = async (
     
-    req: NextApiRequest,
-    res: NextApiResponse
+    cookies: NextRequest["cookies"] | ReadonlyRequestCookies
     ) => {
-        const token = req.cookies['payload-token'];
-
-        if (!token) {
-          // Redirect to sign-in page if the token is not present
-          res.redirect('/sign-in?origin=cart');
-          return { user: null };
-        }
-      
-        const meRes = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/users/me`, {
-          headers: {
-            Authorization: `JWT ${token}`,
-          },
-        });
-      
-        if (!meRes.ok) {
-          console.error('Error fetching user data:', meRes.statusText);
-          // Handle the error, return a default user, or throw an exception as needed.
-          res.status(500).json({ error: 'Internal Server Error' });
-          return { user: null };
-        }
-      
-        try {
-          const { user } = await meRes.json();
-          return { user };
-        } catch (error) {
-          console.error('Error parsing JSON:', error);
-          // Handle the JSON parsing error.
-          res.status(500).json({ error: 'Internal Server Error' });
-          return { user: null };
-        }
+        console.log('server is working correctly')
+        const token = cookies.get("payload-token")?.value
+        
+        const meRes = await fetch(
+            `${process.env.NEXT_PUBLIC_SERVER_URL}/api/users/me`,
+             {
+            headers: {
+                Authorization: `JWT ${token}`,
+            },
+        })
+        console.log('meRes:', meRes);
+    const {user}= (await meRes.json()) as {user: User | null} 
+    return {user}
 }
